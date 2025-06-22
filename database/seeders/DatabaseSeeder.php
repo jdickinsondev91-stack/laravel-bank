d0ebad5f5f808f413b57c602d2241757bd5acfe0
John Dickinson <johndickinson@Johns-Laptop.local>
2025-06-22 15:22:52 +0100

    Forgot to include the account type seeder|

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             CurrencySeeder::class,
             TransactionTypeSeeder::class,
             TransactionSubtypeSeeder::class,
+            AccountTypeSeeder::class,
         ]);
     }
 }
